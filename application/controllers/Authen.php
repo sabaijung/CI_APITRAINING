a91e756d88a317d23c5fc46b1b3438f0dd2780c9
sabaijung <ardsri.s@gmail.com>
2020-02-16 13:37:41 +0700

    เพิ่มฟังก์ชั่นลงทะเบียน

--- application/controllers/Authen.php
+++ application/controllers/Authen.php
@@ -3,6 +3,12 @@ class Authen extends CI_CONTROLLER
 {
     public function __construct()
     {
+		header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Max-Age: 3600");
+		header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+		
 		parent::__construct();
 		//$this->load->model('AuthenModel');
         $this->load->model('AuthenModel','',TRUE);
@@ -14,6 +20,22 @@ class Authen extends CI_CONTROLLER
         $this->output
             ->set_content_type('application/json')
             ->set_output(json_encode($results));
+	}
+	
+	 public function cancelData()
+    {
+		
+	    $result = $this->AuthenModel->cancelData($_GET["username"]);
+        echo json_encode($result);
+	}
+
+	public function register()
+    {
+		$result = $this->AuthenModel->register($_GET['username'],
+					$_GET['password'],$_GET['email']);
+        echo json_encode($result);
     }
+	
+
 }
 ?>
